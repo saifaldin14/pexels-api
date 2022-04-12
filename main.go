a8170df7a94eade52347770d8a4926150d1ca1a2
saifaldin14 <saifaldindeveloper@gmail.com>
2022-04-12 11:36:50 -0400

    Update main.go

--- main.go
+++ main.go
@@ -42,6 +42,68 @@ type Photo struct {
 	Src             PhotoSource `json:"src"`
 }
 
+type CuratedResult struct {
+	Page     int32   `json:"page"`
+	PerPage  int32   `json:"per_page"`
+	NextPage string  `json:"next_page"`
+	Photos   []Photo `json:"photos"`
+}
+
+type PhotoSource struct {
+	Origin    string `json:"original"`
+	Largest   string `json:"large"`
+	Large2x   string `json:"large2x"`
+	Medium    string `json:"medium"`
+	Small     string `json:"small"`
+	Portrait  string `json:"portrait"`
+	Square    string `json:"square"`
+	Landscape string `json:"landscape"`
+	Tiny      string `json:"tiny"`
+}
+
+type VideoSearchResult struct {
+	Page         int32   `json:"page"`
+	PerPage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_results"`
+	NextPage     string  `json:"next_page"`
+	Videos       []Video `json:"videos"`
+}
+
+type Video struct {
+	Id            int32           `json:"id"`
+	Width         int32           `json:"width"`
+	Height        int32           `json:"height"`
+	Url           string          `json:"url"`
+	Image         string          `json:"image"`
+	FullRes       interface{}     `json:"full_res"`
+	Duration      float64         `json:"duration"`
+	VideoFiles    []VideoFiles    `json:"video_files"`
+	VideoPictures []VideoPictures `json:"video_pictures"`
+}
+
+type PopularVideos struct {
+	Page         int32   `json:"page"`
+	PerPage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_results"`
+	Url          string  `json:"url"`
+	Videos       []Video `json:"videos"`
+}
+
+type VideoFiles struct {
+	Id       int32  `json:"id"`
+	Quality  string `json:"quality"`
+	FileType string `json:"file_type"`
+	Width    int32  `json:""width`
+	Height   int32  `json:"height"`
+	Link     string `json:"link"`
+}
+
+type VideoPictures struct {
+	Id      int32  `json:"id"`
+	Picture string `json:"picture"`
+	Nr      int32  `json:"nr"`
+}
+
 func main() {
 	err := godotenv.Load("local.env")
 	if err != nil {
